4ca41086d10a73c7cada6de096e2d71423ab2aba
Ritik Yadav <ritikyadav0178@gmail.com>
2023-10-28 12:33:34 +0530

    sound in bg video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -2,13 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import useTrailerVideo from "../hooks/useTrailerVideo";
 
-const VideoBackground = ({movieId}) => {
-  const trailerVideo = useSelector(store => store.movies?.trailerVideo)
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useTrailerVideo(movieId);
   return (
     <div className="w-screen">
-      <iframe className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?autoplay=1&loop=1&mute=0"}
+      <iframe
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=10"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
